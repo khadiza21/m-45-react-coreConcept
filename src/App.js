adae08b65dd06f5fed9ec9ae27d7155624723b29
khadiza21 <bibikhadiza474@gmail.com>
2022-03-20 19:49:27 +0600

    react app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 // const number = 222;
@@ -19,7 +20,8 @@ function App() {
   const books = ["algorithm", "cpp", "c", "python", "data strcture", "java"];
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+      {/* <Counter></Counter> */}
       {/* <h1>JSX</h1>
         <div className="container">
           <h2>I am from BD</h2>
@@ -34,7 +36,7 @@ function App() {
           </p>
         </div> */}
 
-      {books.map((book) => (
+      {/* {books.map((book) => (
         <li>Name: {book}</li>
       ))}
       {books.map((book) => (
@@ -43,16 +45,52 @@ function App() {
 
       {phones.map((phone) => (
         <Person name={phone.name}></Person>
-      ))}
-      <Person name="khadiza" hobby="Gardening"></Person>
+      ))} */}
+      {/* <Person name="khadiza" hobby="Gardening"></Person>
       <Person name="Tania" hobby="Reading"></Person>
-      <Person name={books[3]} hobby="Robotics"></Person>
+      <Person name={books[3]} hobby="Robotics"></Person> */}
       {/* <Person name="BK" hobby="Drawing"></Person>
       <Person name="Bibi KHadiza" hobby="Problem solving"></Person> */}
       {/* <Person name="BIBI" hobby="coding"></Person>
       <Person name="Vetki" hobby="Hiking"></Person>
       <Person name="Baikka" hobby="Photography"></Person>
       <Person name="Pepe" hobby="Travelling"></Person> */}
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        margin: "20px 30%",
+        padding: "10px",
+        borderRadius: "20px",
+      }}
+    >
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
@@ -67,12 +105,17 @@ function Person(props) {
   );
 }
 
-function Counter(){
-  return(
+function Counter() {
+  const [count, setcount] = useState(0);
+  const increaseCount = () => setcount(count + 1);
+  const decreaseCount = () => setcount(count - 1);
+
+  return (
     <div>
-      <h1>Count: </h1>
-      <button>Increase</button>
+      <h1>Count: {count} </h1>
+      <button onClick={increaseCount}>Increase</button> <br /> <br />
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
-  )
+  );
 }
 export default App;
